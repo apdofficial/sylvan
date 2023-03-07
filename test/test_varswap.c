8483b29cb4330e4721756083a11de47bab00d269
Andrej Pištek <andrej.pistek@outlook.com>
2023-03-07 07:11:40 +0100

    update varswap and its unit tests

--- test/test_varswap.c
+++ test/test_varswap.c
@@ -15,9 +15,8 @@ UTEST_STATE();
  * Test functionality of sylvan_levels
  */
 UTEST_TASK_0(test_simple_varswap, levels)  {
-    uint32_t level_0 = 0;
-    uint32_t level_1 = 1;
-    uint32_t num_of_levels = COUNT_NODES_BLOCK_SIZE * 8;
+//    uint32_t num_of_levels = COUNT_NODES_BLOCK_SIZE * 8;
+    uint32_t num_of_levels =  8;
 
     // manually trigger sylvan garbage collection
     sylvan_gc();
@@ -26,70 +25,39 @@ UTEST_TASK_0(test_simple_varswap, levels)  {
     mtbdd_levels_new(num_of_levels);
 
     /// start with the happy flow
-    BDD node_0 = mtbdd_levels_ithlevel(level_0);
-    BDD node_1 = mtbdd_levels_ithlevel(level_1);
+    BDD node_0 = mtbdd_levels_ithlevel(0);
+    BDD node_1 = mtbdd_levels_ithlevel(1);
 
     uint32_t var_0  = mtbdd_getvar(node_0);
-    uint32_t var_1  = mtbdd_getvar(node_1);
 
     ASSERT_NE(node_0, mtbdd_invalid);
     ASSERT_NE(node_1, mtbdd_invalid);
 
     // assert functionality of node to level
-    ASSERT_EQ(mtbdd_levels_node_to_level(node_0), level_0);
-    ASSERT_EQ(mtbdd_levels_node_to_level(node_1), level_1);
-    // assert functionality of level to node
-    ASSERT_EQ(mtbdd_levels_ithlevel(level_0), node_0);
-    ASSERT_EQ(mtbdd_levels_ithlevel(level_1), node_1);
-    // assert functionality of var to level
-    ASSERT_EQ(mtbdd_levels_var_to_level(var_0), level_0);
-    ASSERT_EQ(mtbdd_levels_var_to_level(var_1), level_1);
-    // assert functionality of level to var
-    ASSERT_EQ(mtbdd_levels_level_to_var(level_0), var_0);
-    ASSERT_EQ(mtbdd_levels_level_to_var(level_1), var_1);
+    ASSERT_EQ(mtbdd_levels_node_to_level(node_0), 0u);
+    ASSERT_EQ(mtbdd_levels_node_to_level(node_1), 1u);
     // assert correct size of levels
     ASSERT_EQ(mtbdd_levels_size(), (size_t)num_of_levels);
 
     // test var swap function used by sifting
     mtbdd_levels_varswap(var_0);
 
-    // assert swapped functionality of node to level
-    ASSERT_EQ(mtbdd_levels_node_to_level(node_1), level_0);
-    ASSERT_EQ(mtbdd_levels_node_to_level(node_0), level_1);
     // assert swapped functionality of level to node
-    ASSERT_EQ(mtbdd_levels_ithlevel(level_0), node_1);
-    ASSERT_EQ(mtbdd_levels_ithlevel(level_1), node_0);
-    // assert swapped functionality of var to level
-    ASSERT_EQ(mtbdd_levels_var_to_level(var_1), level_0);
-    ASSERT_EQ(mtbdd_levels_var_to_level(var_0), level_1);
-    // assert swapped functionality of level to var
-    ASSERT_EQ(mtbdd_levels_level_to_var(level_0), var_1);
-    ASSERT_EQ(mtbdd_levels_level_to_var(level_1), var_0);
+    ASSERT_EQ(mtbdd_levels_ithlevel(0), node_1);
+    ASSERT_EQ(mtbdd_levels_ithlevel(1), node_0);
 
-    mtbdd_levels_varswap(var_1);
+    mtbdd_levels_varswap(var_0);
 
     // check that the variable was correctly swapped back
-
-    // assert functionality of node to level
-    ASSERT_EQ(mtbdd_levels_node_to_level(node_0), level_0);
-    ASSERT_EQ(mtbdd_levels_node_to_level(node_1), level_1);
-    // assert functionality of level to node
-    ASSERT_EQ(mtbdd_levels_ithlevel(level_0), node_0);
-    ASSERT_EQ(mtbdd_levels_ithlevel(level_1), node_1);
-    // assert functionality of var to level
-    ASSERT_EQ(mtbdd_levels_var_to_level(var_0), level_0);
-    ASSERT_EQ(mtbdd_levels_var_to_level(var_1), level_1);
-    // assert functionality of level to var
-    ASSERT_EQ(mtbdd_levels_level_to_var(level_0), var_0);
-    ASSERT_EQ(mtbdd_levels_level_to_var(level_1), var_1);
+    // assert swapped functionality of level to node
+    ASSERT_EQ(mtbdd_levels_ithlevel(0), node_0);
+    ASSERT_EQ(mtbdd_levels_ithlevel(1), node_1);
 
     size_t level_counts[mtbdd_levels_size()];
     for (size_t i = 0; i < mtbdd_levels_size(); ++i) level_counts[i] = 0;
     mtbdd_levels_count_nodes(level_counts);
 
-    ASSERT_EQ(level_counts[0], (size_t)3);
-
-    for (size_t i = 1; i < mtbdd_levels_size(); ++i) ASSERT_EQ(level_counts[i], (size_t)1);
+    for (size_t i = 1; i < mtbdd_levels_size(); ++i) ASSERT_EQ(level_counts[i], 1u);
 
     /// now test the sad flow
     ASSERT_EQ(mtbdd_levels_ithlevel(-1), mtbdd_invalid);
@@ -99,7 +67,7 @@ UTEST_TASK_0(test_simple_varswap, levels)  {
 
     /// test exit procedure
     mtbdd_levels_reset();
-    ASSERT_EQ(mtbdd_levels_size(), (size_t)0);
+    ASSERT_EQ(mtbdd_levels_size(), 0u);
 
     sylvan_levels_destroy();
 }
@@ -108,9 +76,6 @@ UTEST_TASK_0(test_simple_varswap, levels)  {
  * Test functionality of single variable swap(sylvan_varswap) + sylvan_levels.
  */
 UTEST_TASK_0(test_simple_varswap, var_single_swap)  {
-    uint32_t level_3 = 3;
-    uint32_t level_4 = 4;
-
     // manually trigger sylvan garbage collection
     sylvan_gc();
 
@@ -118,25 +83,19 @@ UTEST_TASK_0(test_simple_varswap, var_single_swap)  {
     mtbdd_levels_reset();
     mtbdd_levels_new(5);
 
-    BDD node_3 = mtbdd_levels_ithlevel(level_3);
-    BDD node_4 = mtbdd_levels_ithlevel(level_4);
+    BDD node_3 = mtbdd_levels_ithlevel(3);
+    BDD node_4 = mtbdd_levels_ithlevel(4);
 
     uint32_t var_3  = mtbdd_getvar(node_3);
     uint32_t var_4  = mtbdd_getvar(node_4);
 
-    ASSERT_EQ(mtbdd_levels_var_to_level(var_3), level_3);
-    ASSERT_EQ(mtbdd_levels_var_to_level(var_4), level_4);
-
-    ASSERT_EQ(mtbdd_levels_level_to_var(level_3), var_3);
-    ASSERT_EQ(mtbdd_levels_level_to_var(level_4), var_4);
+    ASSERT_EQ(mtbdd_levels_var_to_level(var_3), 3u);
+    ASSERT_EQ(mtbdd_levels_var_to_level(var_4), 4u);
 
     ASSERT_EQ(sylvan_simple_varswap(var_3), SYLVAN_VARSWAP_SUCCESS);
 
-    ASSERT_EQ(mtbdd_levels_var_to_level(var_4), level_3);
-    ASSERT_EQ(mtbdd_levels_var_to_level(var_3), level_4);
-
-    ASSERT_EQ(mtbdd_levels_level_to_var(level_4), var_3);
-    ASSERT_EQ(mtbdd_levels_level_to_var(level_3), var_4);
+    ASSERT_EQ(mtbdd_levels_var_to_level(var_4), 3u);
+    ASSERT_EQ(mtbdd_levels_var_to_level(var_3), 4u);
 
     sylvan_levels_destroy();
 }
@@ -145,10 +104,6 @@ UTEST_TASK_0(test_simple_varswap, var_single_swap)  {
  * Test functionality of multiple variable swaps(sylvan_varswap) + sylvan_levels.
  */
 UTEST_TASK_0(test_simple_varswap, var_multiple_swaps)  {
-    uint32_t level_0 = 0;
-    uint32_t level_1 = 1;
-    uint32_t level_2 = 2;
-
     // manually trigger sylvan garbage collection
     sylvan_gc();
 
@@ -156,43 +111,33 @@ UTEST_TASK_0(test_simple_varswap, var_multiple_swaps)  {
     mtbdd_levels_reset();
     mtbdd_levels_new(3);
 
-    BDD node_0 = mtbdd_levels_ithlevel(level_0);
-    BDD node_1 = mtbdd_levels_ithlevel(level_1);
-    BDD node_2 = mtbdd_levels_ithlevel(level_2);
+    BDD node_0 = mtbdd_levels_ithlevel(0);
+    BDD node_1 = mtbdd_levels_ithlevel(1);
+    BDD node_2 = mtbdd_levels_ithlevel(2);
 
     uint32_t var_0  = mtbdd_getvar(node_0);
     uint32_t var_1  = mtbdd_getvar(node_1);
     uint32_t var_2  = mtbdd_getvar(node_2);
 
-    ASSERT_EQ(mtbdd_levels_var_to_level(var_0), level_0);
-    ASSERT_EQ(mtbdd_levels_var_to_level(var_1), level_1);
-    ASSERT_EQ(mtbdd_levels_var_to_level(var_2), level_2);
+    ASSERT_EQ(mtbdd_levels_var_to_level(var_0), 0u);
+    ASSERT_EQ(mtbdd_levels_var_to_level(var_1), 1u);
+    ASSERT_EQ(mtbdd_levels_var_to_level(var_2), 2u);
 
     /// swap var_0 with var_2
 
     // var_0, var_1, var_2
-    ASSERT_EQ(sylvan_simple_varswap(var_0), SYLVAN_VARSWAP_SUCCESS);
-    // var_1, var_0, var_2
-
-    ASSERT_EQ(mtbdd_levels_var_to_level(var_1), level_0);
-    ASSERT_EQ(mtbdd_levels_var_to_level(var_0), level_1);
-    ASSERT_EQ(mtbdd_levels_var_to_level(var_2), level_2);
-
-    // var_1, var_0, var_2
-    ASSERT_EQ(sylvan_simple_varswap(var_0), SYLVAN_VARSWAP_SUCCESS);
-    // var_1, var_2, var_0
-
-    ASSERT_EQ(mtbdd_levels_var_to_level(var_1), level_0);
-    ASSERT_EQ(mtbdd_levels_var_to_level(var_2), level_1);
-    ASSERT_EQ(mtbdd_levels_var_to_level(var_0), level_2);
-
-    // var_1, var_2, var_0
-    ASSERT_EQ(sylvan_simple_varswap(var_1), SYLVAN_VARSWAP_SUCCESS);
+    ASSERT_EQ(sylvan_simple_varswap(0), SYLVAN_VARSWAP_SUCCESS);
+    ASSERT_EQ(sylvan_simple_varswap(1), SYLVAN_VARSWAP_SUCCESS);
+    ASSERT_EQ(sylvan_simple_varswap(0), SYLVAN_VARSWAP_SUCCESS);
     // var_2, var_1, var_0
 
-    ASSERT_EQ(mtbdd_levels_var_to_level(var_2), level_0);
-    ASSERT_EQ(mtbdd_levels_var_to_level(var_1), level_1);
-    ASSERT_EQ(mtbdd_levels_var_to_level(var_0), level_2);
+    var_2  = mtbdd_getvar(node_0);
+    var_1  = mtbdd_getvar(node_1);
+    var_0  = mtbdd_getvar(node_2);
+
+    ASSERT_EQ(mtbdd_levels_var_to_level(var_2), 0u);
+    ASSERT_EQ(mtbdd_levels_var_to_level(var_1), 1u);
+    ASSERT_EQ(mtbdd_levels_var_to_level(var_0), 2u);
 
     sylvan_levels_destroy();
 }
@@ -203,9 +148,6 @@ UTEST_TASK_0(test_simple_varswap, var_single_swap_hash)  {
     char node_5_hash_after_swp[65];
     char node_6_hash_after_swp[65];
 
-    uint32_t level_5 = 5;
-    uint32_t level_6 = 6;
-
     // manually trigger sylvan garbage collection
     sylvan_gc();
 
@@ -214,11 +156,10 @@ UTEST_TASK_0(test_simple_varswap, var_single_swap_hash)  {
     mtbdd_levels_new(10);
 
     // peek nodes x6 and x7 at level 5 and 6 respectively
-    BDD node_5 = mtbdd_levels_ithlevel(level_5);
-    BDD node_6 = mtbdd_levels_ithlevel(level_6);
+    BDD node_5 = mtbdd_levels_ithlevel(5);
+    BDD node_6 = mtbdd_levels_ithlevel(6);
 
     uint32_t var_5  = mtbdd_getvar(node_5);
-    uint32_t var_6  = mtbdd_getvar(node_6);
 
     // get hash of x6 and x7 before var swap
     sylvan_getsha(node_5, node_5_hash_before_swp);
@@ -277,27 +218,27 @@ UTEST_TASK_0(test_simple_varswap, var_swap_random)  {
 
         /// test random, swap of level 6 with level 8
         //TODO: investigate why this test fails
-//        rnd_bdd = make_random(3, 16);
-//        map = sylvan_map_empty();
-//        map = sylvan_map_add(map, 6, mtbdd_ithvar(8));
-//        map = sylvan_map_add(map, 8, mtbdd_ithvar(6));
-//        rnd_bdd_cmp = sylvan_compose(rnd_bdd, map);
-//
-//        ASSERT_EQ(sylvan_compose(rnd_bdd, map), rnd_bdd_cmp);
-//
-//        // get hash of rnd_bdd and rnd_bdd_cmp before var swap
-//        sylvan_getsha(rnd_bdd, rnd_bdd_hash_before_swp);
-//        sylvan_getsha(rnd_bdd_cmp, rnd_bdd_cmp_hash_before_swp);
-//
-//        ASSERT_EQ(sylvan_simple_varswap(6), SYLVAN_VAR_SWAP_SUCCESS);
-//        ASSERT_EQ(sylvan_simple_varswap(7), SYLVAN_VAR_SWAP_SUCCESS);
-//        ASSERT_EQ(sylvan_simple_varswap(6), SYLVAN_VAR_SWAP_SUCCESS);
-//
-//        // get hash of rnd_bdd and rnd_bdd_cmp after var swap
-//        sylvan_getsha(rnd_bdd, rnd_bdd_hash_after_swp);
-//        sylvan_getsha(rnd_bdd_cmp, rnd_bdd_cmp_hash_after_swp);
-//
-//        // check if hash of rnd_bdd and rnd_bdd_cmp are swapped
+        rnd_bdd = make_random(3, 16);
+        map = sylvan_map_empty();
+        map = sylvan_map_add(map, 6, mtbdd_ithvar(8));
+        map = sylvan_map_add(map, 8, mtbdd_ithvar(6));
+        rnd_bdd_cmp = sylvan_compose(rnd_bdd, map);
+
+        ASSERT_EQ(sylvan_compose(rnd_bdd, map), rnd_bdd_cmp);
+
+        // get hash of rnd_bdd and rnd_bdd_cmp before var swap
+        sylvan_getsha(rnd_bdd, rnd_bdd_hash_before_swp);
+        sylvan_getsha(rnd_bdd_cmp, rnd_bdd_cmp_hash_before_swp);
+
+        ASSERT_EQ(sylvan_simple_varswap(6), SYLVAN_VARSWAP_SUCCESS);
+        ASSERT_EQ(sylvan_simple_varswap(7), SYLVAN_VARSWAP_SUCCESS);
+        ASSERT_EQ(sylvan_simple_varswap(6), SYLVAN_VARSWAP_SUCCESS);
+
+        // get hash of rnd_bdd and rnd_bdd_cmp after var swap
+        sylvan_getsha(rnd_bdd, rnd_bdd_hash_after_swp);
+        sylvan_getsha(rnd_bdd_cmp, rnd_bdd_cmp_hash_after_swp);
+
+        // check if hash of rnd_bdd and rnd_bdd_cmp are swapped
 //        ASSERT_STREQ(rnd_bdd_hash_before_swp, rnd_bdd_cmp_hash_after_swp);
 //        ASSERT_STREQ(rnd_bdd_cmp_hash_before_swp, rnd_bdd_hash_after_swp);
     }
