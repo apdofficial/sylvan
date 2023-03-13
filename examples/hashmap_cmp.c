482d7a471442d79aee4cf5b5ffed3e05e04c7752
Andrej Pištek <andrej.pistek@outlook.com>
2023-03-13 11:59:59 +0100

    improved hashmap comparison

--- examples/hashmap_cmp.c
+++ examples/hashmap_cmp.c
@@ -4,15 +4,13 @@
 #include <string.h>
 #include <sys/time.h>
 
-#include <sylvan_int.h>
+#include "sylvan_int.h"
 
-static char* to_h(double size, char *buf)
-{
-    const char* units[] = {"B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"};
-    int i = 0;
-    for (;size>1024;size/=1024) i++;
-    sprintf(buf, "%.*f %s", i, size, units[i]);
-    return buf;
+void sylvan_setup(uint64_t memoryCap){
+    sylvan_set_limits(memoryCap, 1, 2);
+    sylvan_init_package();
+    sylvan_init_mtbdd();
+    sylvan_gc_disable();
 }
 
 int compare_float(const void * a, const void * b)
@@ -20,101 +18,93 @@ int compare_float(const void * a, const void * b)
   return ( *(float*)a - *(float*)b );
 }
 
+void calculate_medians(size_t samples_per_round, size_t rounds, float medians[samples_per_round], float data[rounds][samples_per_round]){
+    for (size_t sample = 0; sample < samples_per_round; sample++){
+        // collect all rounds for a given sample
+        float flatten[samples_per_round];
+        
+        for (size_t round = 0; round < rounds; round++){
+            if(data[round][sample] == 0.0f) continue; // skip empty rows
+            flatten[round] = data[round][sample];
+        }
+        // sort the array
+        qsort(flatten, rounds, sizeof(float), compare_float);
+
+        // pick sample from the middle of the distribution
+        medians[sample] = flatten[rounds/2];
+    }
+}
+
 
 TASK_0(int, run)
 {
     size_t rounds = 10;
-    size_t samples_per_round = 400; // expected max
+    size_t samples_per_round = 500; // expected max
 
     float runtimes[rounds][samples_per_round];
     float usages[rounds][samples_per_round];
+    float runtime_medians[samples_per_round];
+    float usage_medians[samples_per_round];
 
+    // run the benchmark
     for (size_t round = 0; round < rounds; ++round){
         printf("round %zu\n", round);
+        sylvan_setup(1LL*1024LLU*1024LLU*1024LLU);
         size_t sample = 0;
-        for (size_t j = 0; j < 50000000; ++j) {
+
+        for (size_t j = 0; j < 50000000; ++j) {       
+
             clock_t start = clock();
-            for (size_t k = 0; k < 40000; ++k) {
-                mtbdd_ithvar(j);
+            for (size_t k = 0; k < 10000; ++k) {
+                MTBDD v = mtbdd_ithvar(j);
+                if (v == mtbdd_invalid) {
+                    printf("table is full\n");
+                    break;
+                }
                 j++;
             }
+            float runtime = clock_ms_elapsed(start);
 
             float used = llmsset_count_marked(nodes);
             float all = llmsset_get_size(nodes);
             float usage = (used/all)*100;
             if (usage >= 99.5) break;
+            
+            // printf("r %zu | s %zu | table usage %.2f%% | runtime: %.2fns\n", round, sample, usage, runtime);
 
             if (round == 0) continue; // warm up round
-            
+
             usages[round-1][sample] = usage;
-            runtimes[round-1][sample] = (float)clock_ms_elapsed(start);
-            // printf("r %zu | s %zu | table usage %.2f%% | runtime: %.2fns\n", round, sample,  usages[round][sample], runtimes[round][sample]);
+            runtimes[round-1][sample] = runtime;
             sample++;
         }
+
         sylvan_quit();
-
-        sylvan_set_limits(2LL*1024LLU*1024LLU*1024LLU, 1, 2);
-        sylvan_init_package();
-        sylvan_init_mtbdd();
-        
-        sylvan_gc_enable();
-        sylvan_gc();
-        sylvan_gc_disable();
-
     }
 
-    FILE *fpt1;
-    fpt1 = fopen("./../../hashmap_cmp_raw.csv", "w+"); // assume cwd is examples 
+    // write the raw data into a csv
+    FILE *file;
+    file = fopen("./hashmap_chaining_raw.csv", "w+");
+    fprintf(file, "round, usages, runtimes_ms\n");
     for (size_t round = 0; round < rounds; round++){
         for (size_t sample = 0; sample < samples_per_round; sample++) {
-            if(runtimes[round][sample] == 0.0f) continue;
-            fprintf(fpt1, "%zu, %.5f, %.5f\n",round, usages[round][sample], runtimes[round][sample]);
+            if(usages[round][sample] == 0.0f) break;
+            fprintf(file, "%zu, %.5f, %.5f\n", round, usages[round][sample], runtimes[round][sample]);
         }
     }
-    fclose(fpt1);
+    fclose(file);
 
-    float usage_medians[samples_per_round];
+    calculate_medians(samples_per_round, rounds, runtime_medians, usages);
+    calculate_medians(samples_per_round, rounds, usage_medians, runtimes);
 
-    for (size_t sample = 0; sample < samples_per_round; sample++){
-        // collect all rounds for a given sample
-        float usage_flatten[samples_per_round];
-        for (size_t round = 0; round < rounds; round++){
-            if(usages[round][sample] == 0.0f) continue;
-            usage_flatten[round] = usages[round][sample];
-        }
-        
-        // sort the array
-        qsort(usage_flatten, rounds, sizeof(float), compare_float);
-
-        printf("s %zu at %zu | median %.4f\n", sample, rounds/2,  usage_flatten[rounds/2]);
-        // pick sample from the middle of the distribution
-        usage_medians[sample] = usage_flatten[rounds/2];
-    }
-
-    float runtime_medians[samples_per_round];
-
-    for (size_t sample = 0; sample < samples_per_round; sample++){
-        // collect all rounds for a given sample
-        float runtime_flatten[samples_per_round];
-        
-        for (size_t round = 0; round < rounds; round++){
-            if(usages[round][sample] == 0.0f) continue;
-            runtime_flatten[round] = runtimes[round][sample];
-        }
-        // sort the array
-        qsort(runtime_flatten, rounds, sizeof(float), compare_float);
-
-        // pick sample from the middle of the distribution
-        runtime_medians[sample] = runtime_flatten[rounds/2];
-    }
-
-    FILE *fpt2;
-    fpt2 = fopen("./../../hashmap_cmp_medians.csv", "w+"); // assume cwd is examples 
+    // write the medians into a csv
+    file = fopen("./hashmap_chaining_medians.csv", "w+");
+    fprintf(file, "usages, runtimes_ms\n");
     for (size_t sample = 0; sample < samples_per_round; sample++) {
-        if(usage_medians[sample] == 0.0f) continue;
-        fprintf(fpt2, "%.5f, %.5f\n", usage_medians[sample], runtime_medians[sample]);
+         if(usage_medians[sample] == 0.0f) break;
+        fprintf(file, "%.5f, %.5f\n", usage_medians[sample], runtime_medians[sample]);
     }
-    fclose(fpt2);
+    fclose(file);
 
     return EXIT_SUCCESS;
 }
@@ -124,20 +114,7 @@ int main(int argc, char **argv)
     // Init Lace
     lace_start(4, 1000000); // auto-detect number of workers, use a 1,000,000 size task queue
 
-    uint64_t memoryCap = 2LL*1024LLU*1024LLU*1024LLU;
-    char buf[32];
-    to_h(memoryCap, buf);
-    printf("Setting Sylvan main tables memory to %s\n", buf);
-
-    sylvan_set_limits(memoryCap, 1, 2);
-    sylvan_init_package();
-    sylvan_init_mtbdd();
-    sylvan_init_reorder();
-    sylvan_gc_disable();
-
     int res = RUN(run);
-
-    sylvan_stats_report(stdout);
 
     lace_stop();
 
