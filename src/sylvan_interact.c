57034bfcaef4b64b074b89799faef56ed8ad2bf8
Andrej Pištek <andrej.pistek@outlook.com>
2023-04-20 17:25:42 +0200

    fully using bitmaps for variable interaction matrix init

--- src/sylvan_interact.c
+++ src/sylvan_interact.c
@@ -39,6 +39,7 @@ void interact_update(interact_t *state, atomic_word_t *bitmap_s, size_t nvars)
             }
         }
     }
+    bitmap_atomic_clear(bitmap_s, nvars - 1);
 }
 
 void interact_print_state(const interact_t *state, size_t nvars)
@@ -77,131 +78,68 @@ void interact_print_state(const interact_t *state, size_t nvars)
  *    / \     / \
  *  F00 F01 F10 F11
  */
-#define find_support(f, support) RUN(find_support, f, support)
-VOID_TASK_2(find_support, MTBDD, f, _Atomic (word_t)*, bitmap_s)
+#define find_support(f, bitmap_s, bitmap_v, bitmap_l) RUN(find_support, f, bitmap_s, bitmap_v, bitmap_l)
+VOID_TASK_4(find_support, MTBDD, f, atomic_word_t *, bitmap_s, atomic_word_t *, bitmap_v, atomic_word_t *, bitmap_l)
 {
     if (mtbdd_isleaf(f)) return;
+    // The low 40 bits are an index into the unique table.
+    uint64_t index = f & 0x000000ffffffffff;
+    if (bitmap_atomic_get(bitmap_l, index) == 1) return;
     mtbddnode_t node = MTBDD_GETNODE(f);
-    if (mtbddnode_getflag(node) == 1) return;
-
+    // set support bitmap, <var> is on the support of <f>
     bitmap_atomic_set(bitmap_s, mtbddnode_getvariable(node));
 
-    SPAWN(find_support, mtbdd_gethigh(f), bitmap_s);
-    CALL(find_support, mtbdd_getlow(f), bitmap_s);
+    SPAWN(find_support, mtbdd_gethigh(f), bitmap_s, bitmap_v, bitmap_l);
+    CALL(find_support, mtbdd_getlow(f), bitmap_s, bitmap_v, bitmap_l);
     SYNC(find_support);
 
     // local visited node used for calculating support array
-    mtbddnode_setflag(node, 1);
+    bitmap_atomic_set(bitmap_l, index);
     // global visited node used to determining root nodes
-    mtbddnode_setvisited(node, 1);
-}
-
-#define clear_flags(f) RUN(clear_flags, f)
-VOID_TASK_1(clear_flags, MTBDD, f)
-{
-    if (mtbdd_isleaf(f)) return;
-    mtbddnode_t node = MTBDD_GETNODE(f);
-    if (mtbddnode_getflag(node) == 0) return;
-
-    SPAWN(clear_flags, mtbdd_gethigh(f));
-    CALL(clear_flags, mtbdd_getlow(f));
-    SYNC(clear_flags);
-
-    mtbddnode_setflag(node, 0);
-}
-
-#define clear_visited() RUN(clear_visited, 0, nodes->table_size)
-VOID_TASK_2(clear_visited, size_t, first, size_t, count)
-{
-    if (count > 1024) {
-        size_t split = count / 2;
-        SPAWN(clear_visited, first, split);
-        CALL(clear_visited, first + split, count - split);
-        SYNC(clear_visited);
-    } else {
-        const size_t end = first + count;
-        for (; first < end; first++) {
-            if (!llmsset_is_marked(nodes, first)) continue; // unused bucket
-            mtbddnode_setvisited(MTBDD_GETNODE(first), 0);
-        }
-    }
-}
-
-char **subtables_malloc()
-{
-    char **subtables = calloc(levels->count, sizeof(char *));
-    if (!subtables) {
-        fprintf(stderr, "interact_malloc failed to allocate new memory!");
-        return NULL;
-    }
-
-    for (size_t i = 0; i < levels->count; i++) {
-        subtables[i] = calloc(nodes->table_size, sizeof(char));
-        if (!subtables[i]) {
-            fprintf(stderr, "interact_malloc failed to allocate new memory!");
-            return NULL;
-        }
-    }
-    return subtables;
-}
-
-void subtables_free(char **subtables)
-{
-    for (size_t i = 0; i < levels->count; i++) {
-        free(subtables[i]);
-        subtables[i] = NULL;
-    }
+    bitmap_atomic_set(bitmap_v, index);
 }
 
 VOID_TASK_IMPL_1(interact_init, interact_t *, state)
 {
-    size_t nnodes = nodes->table_size;
+    size_t nnodes = nodes->table_size; // worst case (if table is full)
     size_t nvars = levels->count;
 
     atomic_word_t *bitmap_s = (atomic_word_t *) alloc_aligned(nvars); // support bitmap
-    // TODO: nnodes * nvars is the worst case (all nodes are of the same variable label), think about improvements
-    atomic_word_t *bitmap_t = (atomic_word_t *) alloc_aligned(nnodes * nvars); // subtables bitmap
+    atomic_word_t *bitmap_v = (atomic_word_t *) alloc_aligned(nnodes); // visited root nodes bitmap
+    atomic_word_t *bitmap_l = (atomic_word_t *) alloc_aligned(nnodes); // locally visited nodes bitmap
 
-    if (bitmap_s == 0 || bitmap_t == 0){
+    if (bitmap_s == 0 || bitmap_v == 0) {
         fprintf(stderr, "interact_init failed to allocate new memory: %s!\n", strerror(errno));
         exit(1);
     }
-    sylvan_init_subtables(bitmap_t);
 
-    for (size_t var = 0; var < levels->count; var++) {
-        for (size_t index = 0; index < nnodes; ++index) {
-            if (bitmap_atomic_get(bitmap_t, var * nvars + index) == 0) continue;
-            if (!llmsset_is_marked(nodes, index)) continue; // unused bucket
-            mtbddnode_t f = MTBDD_GETNODE(index);
-            if (mtbddnode_isleaf(f)) continue;
+    for (size_t index = 0; index < nnodes; ++index) {
+        if (llmsset_is_marked(nodes, index) == 0) continue; // unused bucket
+        if (bitmap_atomic_get(bitmap_v, index) == 1) continue; // already visited node
 
-            // A node is a root of the DAG if it cannot be reached by nodes above it.
-            // If a node was never reached during the previous depth-first searches,
-            // then it is a root, and we start a new depth-first search from it.
-            if (!mtbddnode_getvisited(f)) {
-                assert(mtbddnode_getvariable(f) < levels->count);
-                bitmap_atomic_set(bitmap_s, mtbddnode_getvariable(f));
-                mtbddnode_setvisited(f, 1);
+        mtbddnode_t f = MTBDD_GETNODE(index);
+        // set support bitmap, <var> is on the support of <f>
+        bitmap_atomic_set(bitmap_s, mtbddnode_getvariable(f));
+        // A node is a root of the DAG if it cannot be reached by nodes above it.
+        // If a node was never reached during the previous depth-first searches,
+        // then it is a root, and we start a new depth-first search from it.
+        MTBDD f1 = mtbddnode_gethigh(f);
+        MTBDD f0 = mtbddnode_getlow(f);
 
-                MTBDD f1 = mtbddnode_gethigh(f);
-                MTBDD f0 = mtbddnode_getlow(f);
+        // visit all nodes reachable from <f>
+        SPAWN(find_support, f1, bitmap_s, bitmap_v, bitmap_l);
+        CALL(find_support, f0, bitmap_s, bitmap_v, bitmap_l);
+        SYNC(find_support);
 
-                SPAWN(find_support, f1, bitmap_s);
-                CALL(find_support, f0, bitmap_s);
-                SYNC(find_support);
-
-                SPAWN(clear_flags, f1);
-                CALL(clear_flags, f0);
-                SYNC(clear_flags);
-
-                interact_update(state, bitmap_s, nvars);
-            }
-        }
+        // clear locally visited nodes bitmap
+        memset(bitmap_l, 0, nnodes);
+        // update interaction matrix
+        interact_update(state, bitmap_s, nvars);
     }
 
     free_aligned(bitmap_s, nnodes);
-    free_aligned(bitmap_t, nnodes * nvars);
-    clear_visited();
+    free_aligned(bitmap_v, nnodes);
+    free_aligned(bitmap_l, nnodes);
 }
 
 
